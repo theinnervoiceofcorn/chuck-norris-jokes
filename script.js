4457fe4c34b931a6ecd10b5f584e95855b86ba01
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2024-01-18 00:01:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+async function getChuckNorrisJoke() {
+  try {
+    const url = "https://api.chucknorris.io/jokes/random";
+    const response = await fetch(url);
+    const data = await response.json();
+    const joke = data.value;
+    document.getElementById("jokeText").innerText = joke;
+  } catch (error) {
+    console.error("Error fetching Chuck Norris joke:", error.message);
+  }
+}
+document
+  .getElementById("getJokeButton")
+  .addEventListener("click", getChuckNorrisJoke);
